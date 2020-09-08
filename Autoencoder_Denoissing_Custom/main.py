9b807972a75b1251011c3abe3a5f6971c7f7e750
gelabierto <martingelabertgomez@gmail.com>
2020-09-08 14:49:39 +0200

    Changed some functions operations

--- Autoencoder_Denoissing_Custom/main.py
+++ Autoencoder_Denoissing_Custom/main.py
@@ -11,8 +11,7 @@ import pickle
 import time
 from tensorflow.keras.models import Sequential
 import  varname
-from constant.py import *
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, UpSampling2D
 
 #Use noise_factor 0.0 < x < 1.0
 def add_noise(x_train, x_test, noise_factor):
@@ -23,7 +22,6 @@ def add_noise(x_train, x_test, noise_factor):
     x_train_noisy = np.clip(x_train_noisy, 0., 1.)
     x_test_noisy = np.clip(x_test_noisy, 0., 1.)
     return (x_train_noisy, x_test_noisy )
-
 #Retuns an array of images
 def simple_load():
     IMG_SIZE = 100
@@ -38,7 +36,6 @@ def simple_load():
             pass
     print("Load of ", img, " completed")
     return dataset
-
 #Save test and train images (NO NOISE)
 def save_data(img_train, img_test, var1_name, var2_name):
 
@@ -54,12 +51,23 @@ def save_data(img_train, img_test, var1_name, var2_name):
     pickle_save.close()
     ###END SAVING DATA###
     return 0
-
+#Save test and train images (NO NOISE)
+def save_dataset(dataset, var1_name):
+    file1=var1_name+".pickle"
+    ######SAVING DATA#######
+    pickle_save = open(file1, "wb")
+    pickle.dump(dataset, pickle_save)
+    pickle_save.close()
+    ###END SAVING DATA###
+    return 0
+#FIRST FUNCTION
 def save_for_fist_time():
     IMG_SIZE = 100
     print("This is a demo")
-    training_imgs= simple_load()
-    random.shuffle(training_imgs)
+    dataset= simple_load()
+
+    #Shuffle data for future training
+    random.shuffle(dataset)
 
     img_test = []
     img_train = []
@@ -69,9 +77,18 @@ def save_for_fist_time():
 
     n_level = [0.1,0.2,0.3,0.4]
 
-    for img in training_imgs:
-        img_train.append(img)
-        img_test.append(img)
+    #This have O(n) but Idont care, giving random noise  to the images
+    for img in dataset:
+        noise = random.randrange(len(n_level))
+        x_train_noisy = img + noise * np.random.normal(loc=0.0, scale=1.0, size=img.shape)
+        x_test_noisy = img + noise * np.random.normal(loc=0.0, scale=1.0, size=img.shape)
+
+        x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+        x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+        img_train.append(x_train_noisy)
+        img_test.append(x_test_noisy)
+
 
     # -1 is just for saying how many elements we have? in this case we don't know
     # IMG_SIZE is the dimension of X and Y axis of the image(data)
@@ -81,30 +98,21 @@ def save_for_fist_time():
     img_test = np.array(img_test).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
 
 
-    #Estoy infinitamente seguro que puedo optimizar esto, pero no creo que lo vaya a hacer
-    for n in range(len(n_level)):
-        img_train_n[n],img_test_n[n] =add_noise(img_train, img_test, n_level[n])
-
-    for n in range(len(n_level)):
-        img_train_n[n] = np.array(img_train_n[n]).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
-        img_test_n[n] = np.array(img_test_n[n]).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
-
-
     #QUEDA GUARDAR TODAS LAS IMAGENES CON RUIDO
     save_data(img_train, img_test,varname.varname(img_train), varname.varname(img_test))
-
-
+    save_dataset(dataset,varname.varname(dataset))
 def load_saved_data():
-    pk_opend = open("img_x.pickle", "rb")
+    pk_opend = open("img_train.pickle", "rb")
     x = pickle.load(pk_opend)
 
-    pk_opend = open("label_y.pickle", "rb")
+    pk_opend = open("img_test.pickle", "rb")
     y = pickle.load(pk_opend)
 
-    return x, y
+    pk_opend = open("dataset.pickle", "rb")
+    z = pickle.load(pk_opend)
 
-
-
+    #train, test, original
+    return x, y, z
 def first_train():
 
     # Set CPU as available physical device
@@ -115,83 +123,55 @@ def first_train():
     #tf.debugging.set_log_device_placement(True)
 
     # This is for saving the cnn with a different name eacht time is trained
-    name = "Bird_clas_cnn_64x2-{}".format(int(time.time()))
+    name = "denoissing_catsdogs_clas_cnn_64x2-{}".format(int(time.time()))
 
     tb = keras.callbacks.TensorBoard(log_dir='logs/{}'.format(name))
 
-    img_x, label_y = load_saved_data()
+    img_x, img_y, img_z = load_saved_data()
     print("Se han cargado los datos")
 
+    # train, test, original
     img_x = img_x / 255.0
-    label_y=np.array(label_y)
+    img_y = img_y / 255.0
+    #img_z have the clean images
+    img_z = img_z / 255.0
+
     # print(img_x[1])
 
     #I don't need the 8000 value from the array so, we will omit them with
     print(img_x.shape[1:])
+
     # make model
+    # Autoencoding
     model = Sequential()
+    # Codification
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2), padding='same'))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2), padding='same'))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
 
-    model.add(keras.layers.Conv2D(128, (3, 3), activation= "relu", input_shape=img_x.shape[1:]))
-    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(MaxPooling2D((2, 2), padding='same'))
 
-    model.add(keras.layers.Conv2D(64, (3, 3),activation= "relu"))
-    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(1, (3, 3), activation='relu', padding='same'))
 
-    model.add(keras.layers.Conv2D(64, (3, 3),activation= "relu"))
-    model.add(keras.layers.Conv2D(64, (3, 3), activation="relu"))
-    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.compile(optimizer='adam', loss='mean_squared_error')
 
-    # We need flatter our data because conv2D is in fact
-    # in 2D
-    model.add(keras.layers.Flatten())
-    # Dense layer needs one-D shape
-    model.add(keras.layers.Dense(32,activation= "relu"))
-    model.add(keras.layers.Dense(64, activation= "sigmoid"))
+    model.summary()
 
-    model.compile(loss="categorical_crossentropy",optimizer="adam",metrics =['accuracy'])
-    model.fit(img_x, label_y, epochs=12, batch_size=64,shuffle=True, callbacks=[tb])
+    model.fit(img_x, img_z, epochs=12, batch_size=64,shuffle=True, callbacks=[tb])
 
-    pk_opend = open("table_int.pickle", "rb")
-    table_int= pickle.load(pk_opend)
-
-    pk_opend = open("table_string.pickle", "rb")
-    table_string= pickle.load(pk_opend)
-
-    pk_opend = open("original_imgs.pickle", "rb")
-    original_img = pickle.load(pk_opend)
-
-
-    p=model.predict(img_x)
+    p=model.predict(img_y)
     print(p)
 
-    #The argmax just gets the value that has more probability
-    print(table_string[table_int.index(np.argmax(p[33]))])
-
-    plt.imshow(img_x[33])
     plt.show()
-
-    plt.imshow(original_img[33])
-    plt.show()
-
-    # Drawing images for comparison
-    n = 4
-    plt.figure(figsize=(70, 70))
-    for i in range(n):
-        plt.title(table_string[table_int.index(np.argmax(p[i]))])
-        plt.subplot(2, n, i + 1)
-        plt.imshow(img_x[i], cmap="binary")
-
-        plt.title(table_string[i])
-        plt.subplot(2, n, i + 1 + n)
-        plt.imshow(original_img[i], cmap="binary")
-
-    plt.show()
-
-    model.save('bird_clas_model_newmodel.model')
-    # If it's your first time, save first
-    #save_for_fist_time()
-
-
+    model.save('den_custom_100x100.model')
 def showing_prediction():
 
     img_x, label_y = load_saved_data()
@@ -231,6 +211,13 @@ def showing_prediction():
 
     plt.show()
 
-#showing_prediction()
-first_train()
+
+def main():
+    save_for_fist_time()
+
+if __name__ == "__main__":
+    main()
+
+
+
 
